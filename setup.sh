338a32d3558596697073f0d4db617ef22147c6b3
Panos Korovesis <panoskorovesis@outlook.com>
2024-10-15 00:28:20 +0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# In case of an error stop the script
+set -e
+
+echo "Setting up your environment..."
+
+# Uninstall any existing python virtual environments
+if [ -d ".venv" ]; then
+    echo "Removing existing virtual environment..."
+    rm -rf .venv
+fi
+
+# Create a new python virtual environment using python3.8
+echo "Creating a new virtual environment..."
+python3.10 -m venv .venv
+
+# Update pip
+echo "Updating pip..."
+source .venv/bin/activate
+pip install --upgrade pip
+
+# Install the wheel package, needed for the requirements.txt file
+echo "Installing wheel package..."
+pip install wheel
+
+# Finally install the requirements.txt file
+echo "Installing requirements..."
+
+pip install -r requirements.txt
+
+# Print completion message
+echo "Setup complete!"
