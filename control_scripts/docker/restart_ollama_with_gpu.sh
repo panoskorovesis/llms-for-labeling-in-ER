f5dedad3c7430ad99735b666442f54dcb65e42e1
Panos Korovesis <panoskorovesis@outlook.com>
2024-12-07 11:05:14 +0200

    Add ollama restart control script

---
+++ control_scripts/docker/restart_ollama_with_gpu.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+set -e
+
+# Stop the ollama server
+docker compose -f secure-ollama-server/docker-compose.yaml -f secure-ollama-server/docker-compose.gpu.yaml --env-file secure-ollama-server/.env down
+
+# wait a bit
+sleep 1
+
+# Start the server again
+docker compose -f secure-ollama-server/docker-compose.yaml -f secure-ollama-server/docker-compose.gpu.yaml --env-file secure-ollama-server/.env up -d
