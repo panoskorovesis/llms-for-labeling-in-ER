4a75f0fd15eec9ed5a203a2d29f39db27e56e966
Panos Korovesis <panoskorovesis@outlook.com>
2024-12-09 00:31:27 +0200

    Add sleep to wait for the server to load

--- control_scripts/docker/restart_ollama_with_gpu.sh
+++ control_scripts/docker/restart_ollama_with_gpu.sh
@@ -10,3 +10,6 @@ sleep 1
 
 # Start the server again
 docker compose -f secure-ollama-server/docker-compose.yaml -f secure-ollama-server/docker-compose.gpu.yaml --env-file secure-ollama-server/.env up -d
+
+# finally wait a bit
+sleep 1
